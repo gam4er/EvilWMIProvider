5a286e90f3cc4272360f2b2e5685ed4265b5a4b2
Alexander Rodchenko <Alexander.Rodchenko@kaspersky.com>
2020-03-12 13:38:19 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/TestingApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EvilWMIProvider;
+
+namespace TestingApp
+{
+    class TestingApp
+    {
+        static void Main(string[] args)
+        {
+            /*
+            EvilWMIProvider.EvilInstall install = new EvilInstall() ;
+            System.Collections.IDictionary dictionary = new Dictionary<string,string>();
+            install.Install(dictionary);
+            */
+            var tmp = new EvilWMIProvider.Evil("");
+        }
+    }
+}
---
+++ ConsoleAppCalcPoc/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppCalcPoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process p = System.Diagnostics.Process.Start("cmd.exe", "/c calc.exe && ping -t ya.ru");
+        }
+    }
+}
